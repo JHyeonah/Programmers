1635ef811262d111aa8234d63894613e6ef5b5eb
JHyeonah <gwi02378@gmail.com>
2019-03-24 21:42:22 +0900

    Hash-전화번호 목록

---
+++ phoneBook.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        
+        for(int i=0; i<phone_book.length-1; i++){
+            for(int j=i+1; j < phone_book.length; j++){
+                if(phone_book[j].startsWith(phone_book[i])){
+                    answer = false;
+                }
+            }
+        }
+        
+
+        return answer;
+    }
+}
